990f78cd1549481d77d87c44aaf28c55958ec42e
Justin <justinedwards1230@gmail.com>
2024-04-14 18:35:35 -0400

    fix: auth view background

--- client/src/components/views/auth/AuthViewWrapper.tsx
+++ client/src/components/views/auth/AuthViewWrapper.tsx
@@ -2,8 +2,8 @@ import { KeyboardAvoidingView, View } from "react-native";
 
 export function AuthViewWrapper({ children }: { children: React.ReactNode }) {
 	return (
-		<View className="w-full h-full max-w-full mx-auto text-base sm:max-w-2xl bg-accent text-foreground">
-			<View className="flex justify-center h-full px-0 pb-0 sm:pb-16 sm:px-8">
+		<View className="flex-1 w-full h-full mx-auto text-base bg-accent text-foreground">
+			<View className="flex justify-center w-full h-full max-w-full px-0 pb-0 mx-auto sm:pb-16 sm:max-w-2xl sm:px-8">
 				<KeyboardAvoidingView>{children}</KeyboardAvoidingView>
 			</View>
 		</View>
