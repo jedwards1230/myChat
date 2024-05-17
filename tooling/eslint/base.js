8ad5a0a5a1c6559a5281872cb84490266406b673
Justin <justinedwards1230@gmail.com>
2024-05-17 10:45:50 -0400

    chore: fix docker build

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -16,7 +16,6 @@ const config = {
 	},
 	plugins: ["@typescript-eslint", "import"],
 	rules: {
-		"turbo/no-undeclared-env-vars": "off",
 		"@typescript-eslint/no-unused-vars": [
 			"error",
 			{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
@@ -26,11 +25,13 @@ const config = {
 			{ prefer: "type-imports", fixStyle: "separate-type-imports" },
 		],
 		"@typescript-eslint/no-misused-promises": "off",
-		"import/consistent-type-specifier-style": "off",
+		"@typescript-eslint/no-empty-object-type": "off",
 		"@typescript-eslint/strict-boolean-expressions": 0,
 		"@typescript-eslint/no-explicit-any": 0,
 		"@typescript-eslint/ban-ts-comment": 0,
 		"@typescript-eslint/no-redeclare": "off",
+		"import/consistent-type-specifier-style": "off",
+		"turbo/no-undeclared-env-vars": "off",
 	},
 	ignorePatterns: [
 		"**/.eslintrc.cjs",
